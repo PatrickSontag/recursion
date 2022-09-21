e52f4dce3fc86979a37495a6ab89d4d97f7a7d10
Patrick Sontag <pmsontag84@gmail.com>
2022-09-21 09:13:24 -0500

    isPalindrome function

--- recursion.js
+++ recursion.js
@@ -33,8 +33,10 @@ function everyOther(str, index=0, theString="") {
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {
-
+function isPalindrome(str, leftIndex=0, rightIndex=str.length-1) {
+  if (str[leftIndex] !== str[rightIndex]) return false;
+  if (leftIndex >= rightIndex) return true;
+  return isPalindrome(str, leftIndex + 1, rightIndex - 1);
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
